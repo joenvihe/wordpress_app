7dcaa1e2d749081ec83277a9bf2d956476349b91
joenvihe <enrique@vicenteh.com>
2024-09-22 23:59:26 -0500

    wordpress config

--- wp-config.php
+++ wp-config.php
@@ -51,9 +51,9 @@ if (!defined('ABSPATH')) {
     define('ABSPATH', __DIR__ . '/');
 }
 
-#define('FORCE_SSL_ADMIN', true);
-#if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
-#    $_SERVER['HTTPS']='on';
+define('FORCE_SSL_ADMIN', true);
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+    $_SERVER['HTTPS']='on';
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
